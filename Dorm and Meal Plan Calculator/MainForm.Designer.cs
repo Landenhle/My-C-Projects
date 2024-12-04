202a09c7f54bd66e8ed6f0f0dc5eb46488bfbd7b
Landenhle Zigode <107732827+Landenhle@users.noreply.github.com>
2024-12-04 08:59:14 +0200

    Add files via upload
    Dorm eal calculator application.

---
+++ Dorm and Meal Plan Calculator/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Dorm_and_Meal_Plan_Calculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.allenRadioButton = new System.Windows.Forms.RadioButton();
+            this.pikeRadioButton = new System.Windows.Forms.RadioButton();
+            this.farthingRadioButton = new System.Windows.Forms.RadioButton();
+            this.suitesRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.startersRadioButton = new System.Windows.Forms.RadioButton();
+            this.mainsRadioButton = new System.Windows.Forms.RadioButton();
+            this.unlimitedRadioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.allenRadioButton);
+            this.groupBox1.Controls.Add(this.pikeRadioButton);
+            this.groupBox1.Controls.Add(this.farthingRadioButton);
+            this.groupBox1.Controls.Add(this.suitesRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(75, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 142);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select a Dormitory";
+            // 
+            // allenRadioButton
+            // 
+            this.allenRadioButton.AutoSize = true;
+            this.allenRadioButton.Location = new System.Drawing.Point(6, 27);
+            this.allenRadioButton.Name = "allenRadioButton";
+            this.allenRadioButton.Size = new System.Drawing.Size(69, 17);
+            this.allenRadioButton.TabIndex = 1;
+            this.allenRadioButton.TabStop = true;
+            this.allenRadioButton.Text = "Allen Hall";
+            this.allenRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // pikeRadioButton
+            // 
+            this.pikeRadioButton.AutoSize = true;
+            this.pikeRadioButton.Location = new System.Drawing.Point(6, 50);
+            this.pikeRadioButton.Name = "pikeRadioButton";
+            this.pikeRadioButton.Size = new System.Drawing.Size(67, 17);
+            this.pikeRadioButton.TabIndex = 2;
+            this.pikeRadioButton.TabStop = true;
+            this.pikeRadioButton.Text = "Pike Hall";
+            this.pikeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // farthingRadioButton
+            // 
+            this.farthingRadioButton.AutoSize = true;
+            this.farthingRadioButton.Location = new System.Drawing.Point(6, 73);
+            this.farthingRadioButton.Name = "farthingRadioButton";
+            this.farthingRadioButton.Size = new System.Drawing.Size(84, 17);
+            this.farthingRadioButton.TabIndex = 3;
+            this.farthingRadioButton.TabStop = true;
+            this.farthingRadioButton.Text = "Farthing Hall";
+            this.farthingRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // suitesRadioButton
+            // 
+            this.suitesRadioButton.AutoSize = true;
+            this.suitesRadioButton.Location = new System.Drawing.Point(6, 96);
+            this.suitesRadioButton.Name = "suitesRadioButton";
+            this.suitesRadioButton.Size = new System.Drawing.Size(103, 17);
+            this.suitesRadioButton.TabIndex = 4;
+            this.suitesRadioButton.TabStop = true;
+            this.suitesRadioButton.Text = "University Suites";
+            this.suitesRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.startersRadioButton);
+            this.groupBox2.Controls.Add(this.mainsRadioButton);
+            this.groupBox2.Controls.Add(this.unlimitedRadioButton);
+            this.groupBox2.Location = new System.Drawing.Point(322, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 142);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select A Meal Plan";
+            // 
+            // startersRadioButton
+            // 
+            this.startersRadioButton.AutoSize = true;
+            this.startersRadioButton.Location = new System.Drawing.Point(6, 27);
+            this.startersRadioButton.Name = "startersRadioButton";
+            this.startersRadioButton.Size = new System.Drawing.Size(113, 17);
+            this.startersRadioButton.TabIndex = 1;
+            this.startersRadioButton.TabStop = true;
+            this.startersRadioButton.Text = "7 Meals Per Week";
+            this.startersRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // mainsRadioButton
+            // 
+            this.mainsRadioButton.AutoSize = true;
+            this.mainsRadioButton.Location = new System.Drawing.Point(6, 50);
+            this.mainsRadioButton.Name = "mainsRadioButton";
+            this.mainsRadioButton.Size = new System.Drawing.Size(119, 17);
+            this.mainsRadioButton.TabIndex = 2;
+            this.mainsRadioButton.TabStop = true;
+            this.mainsRadioButton.Text = "14 Meals Per Week";
+            this.mainsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // unlimitedRadioButton
+            // 
+            this.unlimitedRadioButton.AutoSize = true;
+            this.unlimitedRadioButton.Location = new System.Drawing.Point(6, 73);
+            this.unlimitedRadioButton.Name = "unlimitedRadioButton";
+            this.unlimitedRadioButton.Size = new System.Drawing.Size(99, 17);
+            this.unlimitedRadioButton.TabIndex = 3;
+            this.unlimitedRadioButton.TabStop = true;
+            this.unlimitedRadioButton.Text = "Unlimited Meals";
+            this.unlimitedRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show Costs ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(322, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Dorm and Meal Plan";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton allenRadioButton;
+        private System.Windows.Forms.RadioButton pikeRadioButton;
+        private System.Windows.Forms.RadioButton farthingRadioButton;
+        private System.Windows.Forms.RadioButton suitesRadioButton;
+        private System.Windows.Forms.RadioButton unlimitedRadioButton;
+        private System.Windows.Forms.RadioButton startersRadioButton;
+        private System.Windows.Forms.RadioButton mainsRadioButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Dorm and Meal Plan Calculator/MainForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dorm_and_Meal_Plan_Calculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TotalCosts sumCosts = new TotalCosts();
+           
+            double allenHall = 1500;
+            double pikeHall = 1600;
+            double farthingHall = 1800;
+            double universitySuites = 2500;
+
+            double twoMeals = 600;
+            double fourteenMeals = 1200;
+            double unlimitedMeals = 1700;
+
+            try
+            {
+                if(allenRadioButton.Checked && startersRadioButton.Checked)
+                {
+                    double AHTM = allenHall + twoMeals;
+                    sumCosts.textBox1.Text = AHTM.ToString("c");
+                }else if(allenRadioButton.Checked && mainsRadioButton.Checked)
+                {
+                    double AHFM = allenHall + fourteenMeals;
+                    sumCosts.textBox1.Text = AHFM.ToString("c");
+                }else if(allenRadioButton.Checked && unlimitedRadioButton.Checked)
+                {
+                    double AHUM = allenHall + unlimitedMeals;
+                    sumCosts.textBox1.Text = AHUM.ToString("c");
+                }
+                else if (pikeRadioButton.Checked && startersRadioButton.Checked)
+                {
+                    double PHTM = pikeHall + twoMeals;
+                    sumCosts.textBox1.Text = PHTM.ToString("c");
+                }
+                else if (pikeRadioButton.Checked && mainsRadioButton.Checked)
+                {
+                    double PHFM = pikeHall + fourteenMeals;
+                    sumCosts.textBox1.Text = PHFM.ToString("c");
+                }
+                else if (pikeRadioButton.Checked && unlimitedRadioButton.Checked)
+                {
+                    double PHUM = pikeHall + unlimitedMeals;
+                    sumCosts.textBox1.Text = PHUM.ToString("c");
+                }
+                else if (farthingRadioButton.Checked && startersRadioButton.Checked)
+                {
+                    double FHTM = farthingHall + twoMeals;
+                    sumCosts.textBox1.Text = FHTM.ToString("c");
+                }
+                else if (farthingRadioButton.Checked && mainsRadioButton.Checked)
+                {
+                    double FHFM = farthingHall + fourteenMeals;
+                    sumCosts.textBox1.Text = FHFM.ToString("c");
+                }
+                else if (farthingRadioButton.Checked && unlimitedRadioButton.Checked)
+                {
+                    double FHUM = farthingHall + unlimitedMeals;
+                    sumCosts.textBox1.Text = FHUM.ToString("c");
+                }
+                else if (suitesRadioButton.Checked && startersRadioButton.Checked)
+                {
+                    double USTM = universitySuites + twoMeals;
+                    sumCosts.textBox1.Text = USTM.ToString("c");
+                }
+                else if (suitesRadioButton.Checked && mainsRadioButton.Checked)
+                {
+                    double USFM = universitySuites + fourteenMeals;
+                    sumCosts.textBox1.Text = USFM.ToString("c");
+                }
+                else if (suitesRadioButton.Checked && unlimitedRadioButton.Checked)
+                {
+                    double USUM = universitySuites + unlimitedMeals;
+                    sumCosts.textBox1.Text = USUM.ToString("c");
+                }
+
+                sumCosts.ShowDialog();
+            }
+            catch
+            {
+                MessageBox.Show("Please ensure correct options are selected.", "Error");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
